5c91307518832566251e95b46b028ad822d610b1
Atharv Krishn <55579367+AthavKrishn@users.noreply.github.com>
2020-02-02 02:42:24 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="themes/default/css/style.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>
+		WORD_exe
+	</title>
+</head>
+
+<div class="headerFixedBar">
+	<div class="row">
+		<div class="col-lg-12 pageLBar">
+		<div class="pullLeft">
+			<a class="headerLogoText" href="index.php" title="Interact.com"><img class="headerLogoImage" src ="themes/default/images/logo.jpg">Interact</a>
+		</div>
+		<div class="pullRight">
+			<a class="btn btn-primary themeButton" href="index.php?logout" title="Interact.com">Logout</a>
+		</div>
+		</div>	
+	</div>
+</div>
+
+<body>
+	<div class="container forumBackground">
+		<div class="row">
+			<div class="col-lg-12 pageLBar">
+				<a href="#">Home</a> <i class="fa fa-chevron-circle-right pageArrow"></i> <a href="#">Category</a> <i class="fa fa-chevron-circle-right pageArrow"></i> <a href="#">Discussion</a>
+			</div>
+		</div>
+	</div>
+
+
+	<div class="container postBackground">
+		<div class="postSideBar">
+			<div class="votebox">
+  				<div class="up vote"></div>
+  				<div class="down vote"></div>
+			</div>
+		</div>
+		<div class="postTopBar">
+			
+		</div>
+		<div class="post">
+			<div>saoshnvoksuidnvksnvlsdknviskdvihahcsuduidschuudhchuhjjhjjjhjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</div>
+		</div>
+		<div class="postBottemBar">
+			<div class="row">
+			<div class="col-lg-12 pageLBar">
+				<i class="fa fa-comments "></i> <a href="#">Comments </a> <i class="fa fa-share-alt "></i> <a href="#">Share </a> <i class=" fa fa-bookmark-o"></i> <a href="#">Bookmark</a>
+			</div>
+		</div>
+		</div>
+		
+	</div>
+</body>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"type="text/javascript"></script>
+<script
+  src="https://code.jquery.com/jquery-3.4.1.min.js"
+  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+  crossorigin="anonymous"></script>
+</html>
